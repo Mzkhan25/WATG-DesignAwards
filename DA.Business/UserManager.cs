33c23f83cf338d0e92b869703ea17b53a0ed8618
Ahsan Tariq <ahsan.tariq21@gmail.com>
2017-04-13 00:29:12 +0500

    Project renamed/ implemented entity framework data first approach

--- DA.Business/UserManager.cs
+++ DA.Business/UserManager.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DA.Data;
 
 namespace DA.Business
 {
-    public class Class1
+    public class UserManager
     {
+        // create DB context here 
     }
 }
