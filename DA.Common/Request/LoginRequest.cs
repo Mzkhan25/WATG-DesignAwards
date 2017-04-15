4325fb79388a1171a6a4e82fe3c72077f7e17097
Ahsan Tariq <ahsan.tariq21@gmail.com>
2017-04-15 18:01:19 +0500

    basic API structure

--- DA.Common/Request/LoginRequest.cs
+++ DA.Common/Request/LoginRequest.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DA.Common
+namespace DA.Common.Request
 {
-    public class Class1
+    public class LoginRequest
     {
+        public string Pin { get; set; }
     }
 }
---
+++ DA.WebApi/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DA.Common;
+using DA.Common.Response;
+using DA.Common.Request;
+
+namespace DA.WebApi.Controllers
+{
+    [RoutePrefix("api/v1/User")]
+    public class UserController : ApiController
+    {
+        [Route("Login")]
+        [HttpPost]
+        public LoginResponse Login(LoginRequest request)
+        {
+            LoginResponse loginResponse = new LoginResponse();
+
+            return loginResponse;
+        }
+    }
+}
