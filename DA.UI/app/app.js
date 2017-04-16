3800ae8c6d5fab1e926b111716559bbf8f0e33e3
Ahmed Khateeb <khateeb321@gmail.com>
2017-04-16 16:56:49 +0500

    Routes Configuration

--- DA.UI/app/app.js
+++ DA.UI/app/app.js
@@ -1,13 +1,12 @@
 ﻿
-    var app = angular.module('DesignAwardApp', ["ui.router"]);
+    var app = angular.module('DesignAwardApp', ['ui.router']);
 
     app.config(function ($stateProvider, $urlRouterProvider) {
-        debugger;
         $urlRouterProvider
-                       .otherwise('login');
+                       .otherwise('/login');
         $stateProvider
             .state('login', {
-                url: "login",
+                url: "/login",
                 templateUrl: "app/Views/Login.html",
                 controller: "LoginController",
                 controllerAs: "login"
@@ -16,8 +15,3 @@
     app.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
       
     }])
-
-
-    //app.controller('LoginController', function ($scope, $location, $state, $http, $window, $rootScope) {
-
-    //});
